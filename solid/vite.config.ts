3d674b4eebeb80a88603e75282387f7a2d93b608
GuyMichaely <guymichaely@gmail.com>
2026-09-04 19:28:28 -0400

    Make Solid build target production root

--- solid/vite.config.ts
+++ solid/vite.config.ts
@@ -3,7 +3,7 @@ import solid from "vite-plugin-solid";
 
 export default defineConfig({
   root: "solid",
-  base: "/calendar/solid/",
+  base: "/calendar/",
   plugins: [solid()],
   build: {
     outDir: "../site/solid",
