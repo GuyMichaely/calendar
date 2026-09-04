0daa11c356eef92270b7be4043941795bc48b600
GuyMichaely <guymichaely@gmail.com>
2026-09-04 00:51:48 -0400

    Isolate service worker cache lookups by scope

--- site/sw.js
+++ site/sw.js
@@ -44,12 +44,13 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html"))),
+    caches.open(CACHE).then((cache) =>
+      fetch(event.request)
+        .then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        })
+        .catch(() => cache.match(event.request).then((cached) => cached || cache.match("./index.html"))),
+    ),
   );
 });
